be1cefacf1a1bd82ca179056c8bb66553c22a8e0
iamrk-js <javascriptiscoolstuff@gmail.com>
2022-02-14 01:47:31 +0530

    work on reactive form

--- src/app/components/contact-us/contact-us.component.ts
+++ src/app/components/contact-us/contact-us.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-contact-us',
@@ -6,10 +7,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./contact-us.component.scss']
 })
 export class ContactUsComponent implements OnInit {
-
+  public contactForm !: FormGroup;
   constructor() { }
 
   ngOnInit(): void {
+    this.createContactForm()
+  }
+
+  public createContactForm() {
+    this.contactForm = new FormGroup({
+      "name": new FormControl(null, Validators.required),
+      "email": new FormControl(null, [Validators.required, Validators.email]),
+      "subject": new FormControl(null, Validators.required),
+      "message": new FormControl(null, Validators.required)
+    })
+  }
+  onSubmit() {
+    console.log('Form Submitted', this.contactForm);
   }
 
 }
